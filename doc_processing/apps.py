ed423db8e69c48e395e212b3c1bf545fef370bee
Nhunglth <hongnhung16052003@gmail.com>
2025-05-22 23:51:27 +0700

    feat: build basic data for system

--- doc_processing/apps.py
+++ doc_processing/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class DocProcessingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'doc_processing'
+
+    def ready(self):
+        from . import data_initializer
+        data_initializer.build_data_once()
--- doc_processing/utils.py
+++ doc_processing/utils.py
@@ -5,6 +5,13 @@ from qdrant_client.http.models import PointStruct
 from sentence_transformers import SentenceTransformer
 import uuid
 
+import time
+
+def unique_collection_name(name):
+    timestamp = int(time.time() * 1000)  
+    safe_name = name.replace(" ", "_") 
+    return f"{safe_name}_{timestamp}"
+
 def get_text_chunks(text):
   text_splitter = CharacterTextSplitter(
     separator="\n", # tách văn bản tại dấu xuống dòng
@@ -29,10 +36,11 @@ def get_embedding(text_chunks, material):
             vector=emb.tolist(),  # Chuyển về list nếu cần lưu
             payload={
               "text": chunk,
-              "materialId": material['id'],
-              "materialType": material['materialType'],
-              "accessType": material['accessType'],
-              "knowledgeStore": material['knowledgeStore']
+              # "materialId": material['id'],
+              "materialName": material.get('name') or material.get('url'),
+              "materialType": material.get('materialType') or 'url',
+              "accessType": material.get('accessType') or 'public',
+              # "knowledgeStore": material['knowledgeStore']
             }
         ))
 
@@ -56,17 +64,3 @@ def add_points_qdrant(collection_name, points):
     connection.upsert(collection_name=collection_name, points=points)
     
     return True
-
-
-# def get_embedding(text_chunks, model_id="text-embedding-ada-002"):
-#     points = []
-#     for idx, chunk in enumerate(text_chunks):
-#         response = open_ai.Embedding.create(
-#             input=chunk,
-#             model=model_id
-#         )
-#         embeddings = response['data'][0]['embedding']
-#         points.append(PointStruct(id=str(uuid.uuid4()), vector=embeddings,
-#                                    payload={"text": chunk}))
-
-#     return points
