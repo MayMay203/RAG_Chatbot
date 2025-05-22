59343f69fc3fefce4b955efa88c5000882b0dd9b
Nhunglth <hongnhung16052003@gmail.com>
2025-05-23 02:03:29 +0700

    feat: distinguish question context between website URL and file URL

--- chat_user/utils.py
+++ chat_user/utils.py
@@ -1,5 +1,27 @@
 import re
+from urllib.parse import urlparse
+
 def contains_url(text: str) -> bool:
     # Regex đơn giản để phát hiện URL
     url_pattern = r'https?://\S+|www\.\S+'
     return re.search(url_pattern, text) is not None
+
+def extract_first_url(text: str) -> str:
+    match = re.search(r'https?://\S+', text)
+    if not match:
+        return ""
+    url = match.group(0)
+
+    # Loại bỏ ký tự thừa ở cuối (nếu có)
+    url = url.rstrip('.,;:)[]{}\'"<>')
+    
+    return url
+
+def classify_url_type(url: str) -> str:
+    parsed = urlparse(url.lower())
+    path = parsed.path
+
+    if path.endswith((".pdf", ".docx", ".doc", ".pptx", ".xlsx")):
+        return "document"
+    else:
+        return "website"
--- chat_user/views.py
+++ chat_user/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from .llm_model import (get_llm_qdrant, detect_has_context_with_gemini, ask_gemini_with_context)
-from .utils import (contains_url)
+from .utils import (contains_url, extract_first_url, classify_url_type)
 import jwt
 import os
 
@@ -19,8 +19,16 @@ class MessageView(APIView):
             isHasContext = detect_has_context_with_gemini(query)
             if isHasContext:
                 if contains_url(query):
-                    print("Context with URL")
-                    return Response({"type": "context_with_url"})
+                    url = extract_first_url(query)
+                    print(url)
+                    url_type = classify_url_type(url)
+
+                    if url_type == "document":
+                        print("Context with URL (Document)")
+                        return Response({"type": "context_with_url", "url_type": "document", "url": url})
+                    else:
+                        print("Context with URL (Website)")
+                        return Response({"type": "context_with_url", "url_type": "website", "url": url})
                 else:
                     gemini_answer = ask_gemini_with_context(query)
                     return Response(
